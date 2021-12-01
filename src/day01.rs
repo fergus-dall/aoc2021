0aa718d2623bb4e9bb7c2f7421a034e4f9d4a78b
Fergus Dall <sidereal@google.com>
2021-12-01 16:56:31 +1100

    Day 1 finish

--- src/day01.rs
+++ src/day01.rs
@@ -1,14 +1,55 @@
 use std::fs;
 use std::string::String;
+use std::str::FromStr;
 
 fn main() {
     let filename = format!("inputs/{}.txt", module_path!());
     let text = fs::read(filename).ok().expect("Couldn't open file");
     let str = String::from_utf8(text).ok().expect("Could parse UTF8 from file");
 
-    println!("{}", do_stuff());
+    println!("{}", first_part(&str));
+    println!("{}", second_part(&str));
 }
 
-fn do_stuff() -> u32 {
-    0
+fn first_part(s: &str) -> isize {
+    let mut old : isize = -1;
+    let mut result : isize = -1;
+    for part in s.split("\n") {
+        let curr = match FromStr::from_str(part) {
+            Ok(i) => i,
+            Err(_) => continue,
+        };
+
+        if curr > old {
+            result += 1
+        };
+        old = curr;
+    }
+    result
+}
+
+fn second_part(s: &str) -> isize {
+    let mut nums = vec!();
+
+    for part in s.split("\n") {
+        let i : isize = match FromStr::from_str(part) {
+            Ok(i) => i,
+            Err(_) => continue,
+        };
+
+        nums.push(i);
+    }
+
+    let mut old_sum = -1;
+    let mut result = -1;
+    for i in 0..(nums.len()-2) {
+        let new_sum = nums[i] + nums[i+1] + nums[i+2];
+
+        if new_sum > old_sum {
+            result += 1
+        };
+        old_sum = new_sum;
+    }
+
+    result
 }
--- src/template.rs
+++ src/template.rs
@@ -1,14 +1,20 @@
 use std::fs;
 use std::string::String;
+use std::str::FromStr;
 
 fn main() {
     let filename = format!("inputs/{}.txt", module_path!());
     let text = fs::read(filename).ok().expect("Couldn't open file");
     let str = String::from_utf8(text).ok().expect("Could parse UTF8 from file");
 
-    println!("{}", do_stuff());
+    println!("{}", first_part(&str));
+    println!("{}", second_part(&str));
 }
 
-fn do_stuff() -> u32 {
+fn first_part(s: &str) -> isize {
+    0
+}
+
+fn second_part(s: &str) -> isize {
     0
 }
